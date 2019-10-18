5f71240eb43fe5461236b61de81fb55713279246
jamiejamiebobamie <jmccrory@vt.edu>
2019-10-18 14:04:24 -0700

    added more comments

--- DestroySelfWDelay.cs
+++ DestroySelfWDelay.cs
@@ -8,6 +8,15 @@ public class DestroySelfWDelay : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-     Destroy(gameObject, 7f);   
+        
+        /*
+         * Destroy takes two arguments : a gme object to "destroy"
+         * and an optional delay.
+         * the first argument is optional too and the game component that the
+         * the script is attached to will be destroyed
+         * if no GameObject is passed in.
+         */
+
+     Destroy(gameObject, 7f);
     }
 }
